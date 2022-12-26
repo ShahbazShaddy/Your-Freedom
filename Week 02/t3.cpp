ddba8a0022ba635aa6b60b293ad64b7f13bf27d1
ShahbazShaddy <121121677+ShahbazShaddy@users.noreply.github.com>
2022-12-26 09:08:53 -0800

    Week 02 PD

---
+++ Week 02/t3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(){
+cout <<"     * A                          " <<endl;
+cout <<"     * X                          " <<endl;
+cout <<"     * Z                           " <<endl;
+cout <<"     * K                          " <<endl;
+cout <<"     * S                          " <<endl;
+cout <<"     * U                          " <<endl;
+}
---
+++ Week 02/t7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main(){               
+
+cout <<"                 .::---::.." <<endl;
+cout <<"              .-------------." <<endl;
+cout <<"            .-----------------." <<endl;
+cout <<"            ----------------:." <<endl;
+cout <<"           :-----------::." <<endl;
+cout <<"           -----------:." <<endl;
+cout <<"           :-------------:.." <<endl;
+cout <<"            :----------------:." <<endl;
+cout <<"             .---------------:" <<endl;
+cout <<"               .:---------:." <<endl;
+system("color 17");
+}
